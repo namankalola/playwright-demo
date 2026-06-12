abd073e3098dab644cbfc469a2da1ad8924fb2ba
KeyurVadhadia111 <keyurvadhadia111@gmail.com>
2026-06-12 17:57:05 +0530

    Cucumber runner changes

--- features/stepdefinitions/hooks.js
+++ features/stepdefinitions/hooks.js
@@ -1,15 +1,30 @@
 const { Before, After, BeforeStep, AfterStep, Status, setDefaultTimeout } = require('@cucumber/cucumber');
 const { POManager } = require('../../pageobjects/POManager');
 const playwright = require('@playwright/test');
+const { DefaultAzureCredential } = require('@azure/identity');
+const { getConnectOptions, ServiceOS } = require('@azure/playwright');
 
-setDefaultTimeout(60 * 1000);
+setDefaultTimeout(5 * 60 * 1000);
 
 Before(async function () {
-    this.browser = await playwright.chromium.launch({ headless: false });
+    if (process.env.PLAYWRIGHT_SERVICE_URL) {
+        const { wsEndpoint, options } = await getConnectOptions({
+            os: ServiceOS.LINUX,
+            connectTimeout: 3 * 60 * 1000,
+            exposeNetwork: '<loopback>',
+            credential: new DefaultAzureCredential()
+        });
+
+        this.browser = await playwright.chromium.connect(wsEndpoint, options);
+        console.log("Connected to Azure Playwright browser");
+    } else {
+        this.browser = await playwright.chromium.launch({ headless: false });
+        console.log("Local browser launched successfully");
+    }
+
     this.context = await this.browser.newContext();
     this.page = await this.context.newPage();
     this.poManager = new POManager(this.page);
-    console.log("Browser launched successfully");
 });
 
 After(async function () {
